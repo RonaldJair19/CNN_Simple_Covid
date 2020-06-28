bf91eaab8b0782858c052b985236a271dc6108b0
Ronald Juarez <jairudas1998@gmail.com>
2020-06-27 23:18:53 -0500

    CNN Funcional

--- cnn_proyecto_covid.py
+++ cnn_proyecto_covid.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras import optimizers
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
-from tensorflow.python.keras import backend as K 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Activation, Convolution2D, MaxPooling2D
+from tensorflow.keras import backend as K 
+from tensorflow.keras.optimizers import Adam
 
 K.clear_session()
 
@@ -15,7 +16,7 @@ path_validacion = 'E:/Documentos/UTP/Cuarto_Anio/Sistemas_basados_en_el_conocimi
 #Declarando los parametros de la red neuronal
 epocas = 20
 altura, longitud = 250, 250
-batch_size = 32
+batch_size = 20
 pasos = 1000
 pasos_de_validacion = 200
 FConv1 = 32
@@ -34,9 +35,11 @@ data_gen_entrenamiento = ImageDataGenerator(
 	horizontal_flip = True
 )
 
-data_gen_validacion(
+data_gen_validacion = ImageDataGenerator(
 	rescale = 1./225
 	)
+
+
 imagen_entrenamiento = data_gen_entrenamiento.flow_from_directory(
 	path_entrenamiento,
 	target_size = (altura, longitud),
@@ -54,11 +57,11 @@ imagen_validacion = data_gen_validacion.flow_from_directory(
 #Se crea la red convolucional
 cnn = Sequential()
 #Primera capa
-cnn.add(Convolution2D(FConv1, tam_fil1, padding = 'same', input_shape = (altura, longitud), activation = 'relu'))
+cnn.add(Convolution2D(FConv1, tam_fil1, padding = 'same', input_shape = (altura, longitud,3), activation = 'relu'))
 #Segunda Capa
 cnn.add(MaxPooling2D(pool_size = tam_pool))
 #Tercera capa
-cnn.add(FConv2, tam_fil2, padding = 'same', activation = 'relu')
+cnn.add(Convolution2D(FConv2, tam_fil2, padding = 'same', activation = 'relu'))
 #Cuarta capa
 cnn.add(MaxPooling2D(pool_size = tam_pool))
 #Quinta Capa, aplana la imagen
@@ -69,3 +72,18 @@ cnn.add(Dense(256, activation = 'relu'))
 cnn.add(Dropout(0.5))
 #Octava capa densa
 cnn.add(Dense(clases, activation = 'softmax'))
+#Copilacin
+cnn.compile(loss = 'categorical_crossentropy', optimizer = optimizers.Adam(lr = lr, beta_1 = 0.5 ), metrics = ['accuracy'])
+
+cnn.fit(imagen_entrenamiento, steps_per_epoch = pasos, epochs = epocas, validation_data = imagen_validacion, validation_steps = pasos_de_validacion)
+
+
+#Guardando el modelo
+dir = 'E:/Documentos/UTP/Cuarto_Anio/Sistemas_basados_en_el_conocimiento/Proyecto/Modelo'
+
+if not os.path.exists(dir):
+	os.mkdir(dir)
+
+cnn.save('E:/Documentos/UTP/Cuarto_Anio/Sistemas_basados_en_el_conocimiento/Proyecto/Modelo/modelo_prueba.h5')
+cnn.save_weights('E:/Documentos/UTP/Cuarto_Anio/Sistemas_basados_en_el_conocimiento/Proyecto/Modelo/pesos_modelo.h5')
+
